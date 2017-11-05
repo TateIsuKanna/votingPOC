7a8021ce537b9686fe12554b44291eac04878dac
TateIsuKanna <TateIsuKanna@users.noreply.github.com>
2017-11-06 00:50:05 +0900

    テストファイル

---
+++ cgi.py
@@ -0,0 +1,12 @@
+
+#!/data/data/com.termux/files/usr/bin/python
+print('Content-type: text/html; charset=UTF-8\r\n')
+print("<meta name='viewport' content='width=device-width, initial-scale=1'>")
+print("<form>")
+print("<fieldset>")
+print("<p><label><input type='radio' name='chara' value='日向縁'>日向縁</label></p>")
+print("<p><label><input type='radio' name='chara' value='櫟井唯'>櫟井唯</label></p>")
+print("<p><label><input type='radio' name='chara' value='野々原ゆずこ'>野々原ゆずこ</label></p>")
+print("</fieldset>")
+print("</form>")
+
